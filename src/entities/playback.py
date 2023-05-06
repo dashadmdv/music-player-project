2ed3a0aa810bc23850d5948426bda8afc8f5d8db
dashadmdv <apushkins7@gmail.com>
2023-05-06 22:28:43 +0300

    add: playback iteration to next/previous song

--- src/entities/playback.py
+++ src/entities/playback.py
@@ -1,20 +1,31 @@
 import vlc
+from src.entities.queue import Queue
 
 
 class Playback(object):
     def __init__(self):
         self.player = vlc.MediaPlayer()
         self.paused = False
+        self.queue = Queue()
 
     def __new__(cls):
         if not hasattr(cls, 'instance'):
             cls.instance = super(Playback, cls).__new__(cls)
         return cls.instance
 
-    def play(self, source: str):
-        media = vlc.Media(source)
-        self.player.set_media(media)
-        self.player.play()
+    def play(self, song: tuple, playlist: list = []):
+        if not song[1]:
+            self.next()
+        else:
+            if self.queue.is_empty() and not playlist:
+                self.queue.update(song)
+            elif self.queue.is_empty() and playlist:
+                self.queue.reload(song, playlist)
+            else:
+                self.queue.reload(song, playlist)
+            media = vlc.Media(song[1])
+            self.player.set_media(media)
+            self.player.play()
 
     def pause(self):
         self.paused = not self.paused
@@ -23,3 +34,25 @@ class Playback(object):
     def stop(self):
         self.paused = False
         self.player.stop()
+
+    def previous(self):
+        prev = self.queue.previous()
+        self.queue.update(prev)
+        if not prev[1]:
+            self.previous()
+        else:
+            self.stop()
+            media = vlc.Media(prev[1])
+            self.player.set_media(media)
+            self.player.play()
+
+    def next(self):
+        next = self.queue.next()
+        self.queue.update(next)
+        if not next[1]:
+            self.next()
+        else:
+            self.stop()
+            media = vlc.Media(next[1])
+            self.player.set_media(media)
+            self.player.play()
