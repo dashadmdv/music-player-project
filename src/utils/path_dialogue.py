6078cd8e293be2032ff0a07bf2e8de5c5cef9d5c
dashadmdv <apushkins7@gmail.com>
2023-05-22 19:12:54 +0300

    add: selecting local music source directories

---
+++ src/utils/path_dialogue.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter.filedialog import askdirectory
+
+
+def select_path():
+    window = tk.Tk()
+    window.wm_attributes('-topmost', 1)
+    window.withdraw()
+    path = askdirectory(parent=window, title='Select folder', mustexist=True)
+    return path
